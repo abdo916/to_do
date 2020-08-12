15b7b263751c918d1716834f3b5b6549871cd5e1
unknown <abdallah.gibreel@gmail.com>
2020-08-12 18:58:10 +0200

    Updates

---
+++ precache-manifest.91ac7bb20c1cd831dbadf9eef01eda84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66e61ae92cdb4a5ceee45766056f86d",
+    "url": "/to_do/index.html"
+  },
+  {
+    "revision": "5c83cb8c0c6cf434d56c",
+    "url": "/to_do/static/css/main.70bd2c4d.chunk.css"
+  },
+  {
+    "revision": "a903e1b8f032859213f8",
+    "url": "/to_do/static/js/2.ffd1124d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to_do/static/js/2.ffd1124d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c83cb8c0c6cf434d56c",
+    "url": "/to_do/static/js/main.9e102ac3.chunk.js"
+  },
+  {
+    "revision": "5006ed160faf45f1396f",
+    "url": "/to_do/static/js/runtime-main.c77d52ba.js"
+  }
+]);
